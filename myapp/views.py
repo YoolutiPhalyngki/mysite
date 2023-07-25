bcedb056d47e6e5b2141a7c0c5cade1c03791452
Yooluti Phalyngki <lutiyoo@gmail.com>
2023-07-25 13:07:33 +0530

    create Product model, view and templates

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
 
 def products(request):
-    products = ["iphone", "imac", "ipad"]
-    return HttpResponse(products)
+    products = Product.objects.all()
+    return render(request, 'myappp/index.html')
